043e51352e08af9b9adb8a98de51e9872c6c1620
Matheus <matheus_batista@discente.ufg.br>
2021-08-24 21:12:12 -0300

    Update Ex1.java

--- java-algoritimo/Ex1.java
+++ java-algoritimo/Ex1.java
@@ -1,6 +1,26 @@
 public class Ex1{
     public static void main(String[] args){
-        System.out.println("testeee");
-        //teste
+        //int a;
+
+        //int b;       
+
+        //a = 3 + 2 * (b = 7/2);
+
+        //System.out.printf("a = %d e b = %d",a,b);      
+        
+        /*int a=1;
+
+        int b=2;        
+
+        b %= a+a+a;       
+
+        System.out.printf("b=%d\n",b);    
+        */
+
+        int a=1; int b=2; int c=3;
+
+        a += b+= c+= 7;
+
+       System.out.printf("a=%d, b=%d, c=%d",a,b,c);  
     }
 }
